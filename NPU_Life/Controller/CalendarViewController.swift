4b5410d0196310ee8ae84529de353e66ba3afb68
biganna <a29174332@hotmail.com>
2021-08-06 15:27:57 +0800

    change api key

--- NPU_Life/Controller/CalendarViewController.swift
+++ NPU_Life/Controller/CalendarViewController.swift
@@ -97,7 +97,7 @@ class CalendarViewController: UIViewController{
     
     func getCalenderList(searchItems query:String = "",queryYear year:String , queryMonth month:String){
         self.dolphinLoading.isHidden = false
-        let key = "[YOUR GOOGLE API KEY]"
+        let key = "CHANGE TO YOUR GOOGLE API KEY"
         let calID = "gms.npu.edu.tw_dktg99t2p2sqlvcoorpd67h7ok@group.calendar.google.com"
         let queryMoneth = "\(year)-\(month)"
         let startTime = "\(queryMoneth)-01T00:00:00Z"
